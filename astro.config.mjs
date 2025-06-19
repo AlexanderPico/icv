e0a12302e541d27c73e34580ef89cc4a32a4286c
Alexander Pico <alex.pico@gladstone.ucsf.edu>
2025-06-18 17:37:36 -0700

    another

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   // site: "https://<USERNAME>.github.io/icv", // Uncomment and set your production URL if needed
+  base: process.env.BASE_PATH || '/',
   integrations: [],
 }); 
